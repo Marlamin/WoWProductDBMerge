8b677a5f930447e7e24662ff57c918603fe234e9
Martin Benjamins <ictmartin@gmail.com>
2019-01-11 20:49:11 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using ProtoBuf;
+using ProtoDatabase;
+
+namespace productDBTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new Database();
+            var retailDB = Serializer.Deserialize<ProductInstall>(File.OpenRead(@"C:\Program Files (x86)\World of Warcraft\.product.db"));
+            var ptrDB = Serializer.Deserialize<ProductInstall>(File.OpenRead(@"C:\Program Files (x86)\World of Warcraft Public Test\.product.db"));
+            ptrDB.Settings.InstallPath = retailDB.Settings.InstallPath;
+
+            db.productInstalls.Add(retailDB);
+            db.productInstalls.Add(ptrDB);
+
+            using (var stream = new MemoryStream())
+            {
+                Serializer.Serialize(stream, db);
+                File.WriteAllBytes(".product.db.movemetowowfolder", stream.ToArray());
+            }
+        }
+    }
+}
